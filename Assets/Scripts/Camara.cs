46a3365c00d0af6402fcd8cd7a678fc55944f8ae
kogarashi <brayanarellano1605@gmail.com>
2024-03-26 20:13:14 -0500

    Botones principales menu inicial

--- Assets/Scripts/Camara.cs
+++ Assets/Scripts/Camara.cs
@@ -1,10 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Camara : MonoBehaviour
 {
-    
     public float sensityivity = 1f;
     public CharacterController playerCharacterController;
     float cameraVerticalRotation = 0f;
@@ -13,11 +10,7 @@ public class Camara : MonoBehaviour
     void Start()
     {
         playerCharacterController = GetComponentInParent<CharacterController>();
-        Cursor.visible = false;
-        if (lockedCursor)
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
+        HideCursor();
     }
 
     // Update is called once per frame
@@ -25,10 +18,22 @@ public class Camara : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
-
         cameraVerticalRotation -= mouseY * sensityivity * 100 * Time.deltaTime;
         cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
         transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
         playerCharacterController.transform.Rotate(Vector3.up * mouseX);
+    }
+
+    public void HideCursor()
+    {
+        lockedCursor = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void ShowCursor()
+    {
+        lockedCursor = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 }
--- Assets/Scripts/MenuActions.cs
+++ Assets/Scripts/MenuActions.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuActions : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Jugador player;
     void Start()
     {
-        
-    }
+        player = GameObject.FindWithTag("Player").GetComponent<Jugador>();
+        player.gravityMultiplier = 0;
+        player.GetComponentInChildren<Camera>().GetComponent<Camara>().ShowCursor();
+        GameObject.Find("SettingsMenu")?.SetActive(false);
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameObject.Find("SettingsMenu")?.SetActive(false);
+        }
+    }
+    public void CloseGame()
+    {
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit(0);
+        }
+    }
+
+    public void NewGame(){
+        SceneManager.LoadScene("Maze3x3");
     }
 }
